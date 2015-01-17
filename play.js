ff0582c4f3595873d61e3a10d9306d9931fd4c28
Noah Bass <bassjnoah@gmail.com>
2015-01-17 17:54:10 -0500

    renamed index.js to play.js

---
+++ play.js
@@ -0,0 +1,32 @@
+var dotenv  = require('dotenv');
+var lame    = require('lame');
+var Speaker = require('speaker');
+var Spotify = require('spotify-web');
+
+dotenv.load();
+
+var uri = process.argv[2] || 'spotify:track:0ml0C3EBDZyRhUrT3p5cMu';
+
+// Spotify credentials from `.env`
+var username = process.env.USERNAME;
+var password = process.env.PASSWORD;
+
+Spotify.login(username, password, function(err, spotify) {
+    if(err) throw err;
+
+    // first get a "Track" instance from the track URI
+    spotify.get(uri, function(err, track) {
+        if(err) throw err;
+
+        console.log('Playing: %s - %s', track.artist[0].name, track.name);
+
+        // play() returns a readable stream of MP3 audio data
+        track.play()
+            .pipe(new lame.Decoder())
+            .pipe(new Speaker())
+            .on('finish', function () {
+                spotify.disconnect();
+        });
+
+    });
+});
